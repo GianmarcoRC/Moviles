5ce6fd24c778f4689166783119d4a72fbafbc5d8
GianmarcoRC <gianmarcorm2005@gmail.com>
2025-05-05 12:23:27 +0200

    Añadido productos

--- MiTiendaPokemon/app/src/main/java/com/example/mitienda/recicler/ProductAdapter.kt
+++ MiTiendaPokemon/app/src/main/java/com/example/mitienda/recicler/ProductAdapter.kt
@@ -1,38 +1,33 @@
 package com.example.mitienda.recicler
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mitienda.R
 import com.example.mitienda.model.Product
 
-class ProductAdapter(private val onProductoClick: (Product) -> Unit) : RecyclerView.Adapter<ProductView>() {
+class ProductAdapter(private val onProductClick: (Product) -> Unit) : RecyclerView.Adapter<ProductView>() {
 
     private var productos: List<Product> = emptyList()
-    private var categoriasVisibles = mutableSetOf<Long>()
 
     fun setProductos(nuevosProductos: List<Product>) {
         productos = nuevosProductos
-        categoriasVisibles.clear()
-        productos.forEach { producto ->
-            categoriasVisibles.add(producto.categoryId)
-        }
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductView {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_product, parent, false)
-        return ProductView(view, onProductoClick)
+        return ProductView(view, onProductClick)
     }
 
     override fun onBindViewHolder(holder: ProductView, position: Int) {
         val producto = productos[position]
-
-        val mostrarEncabezadoCategoria = position == 0 ||
-                producto.categoryId != productos[position - 1].categoryId
-
-        holder.bind(producto, mostrarEncabezadoCategoria)
+        holder.bind(producto)
     }
 
     override fun getItemCount(): Int = productos.size
